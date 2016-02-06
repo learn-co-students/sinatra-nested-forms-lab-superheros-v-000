77095e79aabcb12b00c6ccb89a270450abad6e57
Tsundu <osmentdan@gmail.com>
2016-02-05 20:16:06 -0500

    added nested forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,25 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+      erb :super_hero
+
+    end 
+
+    post '/teams' do 
+      @team = Team.new(params[:team])
+
+      params[:team][:members].each do |value|
+         
+         SuperHero.new(value)
+
+      end 
+      @hero = SuperHero.all 
+      
+      erb :team
+    end 
+
+
+
 
 end
