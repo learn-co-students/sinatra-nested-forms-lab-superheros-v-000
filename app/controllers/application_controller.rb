293196276a6fad17cfbe35a3be2004dc75304c6c
Dakota Martinez <dakotaleemusic@gmail.com>
2016-02-09 06:20:31 +0000

    adds routes to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
+require_relative "../models/hero.rb"
+require_relative "../models/team.rb"
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  get "/" do 
+    erb :super_hero
+  end
+  
+  post "/teams" do 
+    @team = Team.new(params[:team])
+    
+    @members.each do |member, details|
+      Hero.new(details)
+    end
+    
+    @heroes = Hero.all
+    erb :team
+  end
 
 end
