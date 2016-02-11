c05a92b390af54991e81f0d9e442f408ff9430a0
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-02-11 01:34:20 +0000

    completed.

---
+++ app/models/heros.rb
@@ -0,0 +1,11 @@
+class Heros
+   
+  attr_accessor :name, :power, :biography
+   
+  def initialize(params)
+    @name=params[:name]
+    @power=params[:power]
+    @biography=params[:biography]
+  end
+  
+end
