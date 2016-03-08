fcfe9664a5b98832a2e7f43fd101f063bc4a404b
ed-karabinus <ejkarabin@gmail.com>
2016-03-08 02:33:56 -0600

    Finished Sinatra Nested Forms Lab (Superheros)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 require 'sinatra/base'
+require_relative '../models/team'
+require_relative '../models/hero'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :views, Proc.new { File.join(root, "..", "views") }
 
+    get '/' do 
+      erb :super_hero
+    end
 
+    post '/teams' do 
+      @team = Team.new(name: params["team"]["name"], motto: params["team"]["motto"])
+      params["team"]["heroes"].each do |hero|
+        Hero.new(name: hero["name"], power: hero["power"], biography: hero["biography"])
+      end
+      @hero_1 = Hero.all[-3]
+      @hero_2 = Hero.all[-2]
+      @hero_3 = Hero.all[-1]
+      erb :team
+    end
 end
---
+++ app/models/team.rb
@@ -0,0 +1,8 @@
+class Team
+  attr_accessor :name, :motto
+
+  def initialize(args)
+    self.name = args[:name]
+    self.motto = args[:motto]
+  end
+end
