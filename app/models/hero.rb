0641c5c25c40d36f1f3b29aeed7eeed7b682d06e
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-04-07 10:33:48 -0700

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,13 @@
+class Hero
+  attr_accessor :name, :power, :bio
+  HEROS = []
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    HEROS << self
+  end
+  def self.all
+    HEROS
+  end
+end
