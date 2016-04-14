ee74ce3106967a9d1b4dfa93f7c4e2c801c15b72
mirandalily <mirandaraymond@me.com>
2016-04-14 11:42:39 -0700

    Done.

---
+++ app/models/member.rb
@@ -0,0 +1,17 @@
+class Members
+  attr_accessor :name, :power, :bio
+
+  MEMBERS = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    MEMBERS << self
+  end
+
+  def self.all
+    MEMBERS
+  end
+
+end
