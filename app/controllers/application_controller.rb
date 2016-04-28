4f37057d4221648c5ec2fd9dae5d98938f9f324e
abadfish <kirsten.ofarrell@gmail.com>
2016-04-28 13:30:16 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
   post '/teams' do
     @team = Team.new(params[:team])
-    params[:team][:heroes].each do |hero|
+    params[:team][:members].each do |hero|
       Hero.new(hero)
     end
     @heroes = Hero.all
