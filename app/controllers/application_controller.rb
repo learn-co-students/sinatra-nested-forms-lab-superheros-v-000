f5e381d6d653a9a11df3dcaad4231fe2dca133f4
ldgarber <leah.garber88@gmail.com>
2016-04-27 21:06:11 -0400

    display form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+      erb :super_hero
+    end
 
+    post '/teams' do 
+      @team = Team.new(params[:team][:name], params[:team][:motto]) 
+
+      params[:team][:hero].each do |hero|
+        Superhero.new(hero[:name], hero[:power], hero[:bio])
+      end
+
+      @heroes = Superhero.all
+
+      erb :teams
+    end
 end
---
+++ app/models/team.rb
@@ -0,0 +1,14 @@
+class Team
+  attr_accessor :name, :motto
+  @@teams = []
+
+  def initialize(name, motto)
+    @name = name
+    @motto = motto
+    @@teams << self
+  end
+
+  def self.all
+    @@teams
+  end
+end
