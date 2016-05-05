8cfa3d8300c63d2df98256db23d5627f754e319f
joshnevius <joshnevius92@gmail.com>
2016-05-05 18:47:27 -0400

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,15 @@
+class Superhero
+  attr_accessor :name, :power, :bio
+  @@superheroes = []
+ 
+  def initialize(name, power, bio)
+    @name = name
+    @power = power 
+    @bio = bio
+    @@superheroes << self
+  end
+
+  def self.all
+    @@superheroes
+  end
+end
