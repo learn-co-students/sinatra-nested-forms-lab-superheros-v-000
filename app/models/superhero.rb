0f0e2caa0766e4db632117f71ca0faebd83a9206
jbok4 <jbok4@aol.com>
2016-05-13 17:13:41 -0400

    complete

---
+++ app/models/superhero.rb
@@ -0,0 +1,19 @@
+class SuperHero
+
+  attr_accessor :name, :power, :bio
+
+  @@superheroes = []
+
+  def initialize(details)
+    @name = details[:name]
+    @power = details[:power]
+    @bio = details[:bio]
+    @@superheroes << self
+  end
+
+  def self.all
+    @@superheroes
+  end
+
+
+end
