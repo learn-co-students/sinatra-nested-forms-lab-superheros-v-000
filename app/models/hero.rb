a33a37b2a1fddd021e4e049905c892c4c850fd94
Savannah Scott <sascott624@gmail.com>
2016-06-15 14:00:35 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,16 @@
+class Hero
+  attr_accessor :name, :power
+
+  @@all = []
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @power = attributes[:power]
+    @bio = attributes[:bio]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
---
+++ app/models/team.rb
@@ -0,0 +1,8 @@
+class Team
+  attr_accessor :name, :motto
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @motto = attributes[:motto]
+  end
+end
