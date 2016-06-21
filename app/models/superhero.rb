cf9e5a43d616530beea4a93c39bf284a4619b008
Christian Whitesides <whitesidescr@gmail.com>
2016-06-21 15:05:42 -0600

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,16 @@
+class Superhero
+  attr_reader :name, :power, :bio
+
+  SUPERHEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    SUPERHEROES << self
+  end
+
+  def self.all
+    SUPERHEROES
+  end
+end
