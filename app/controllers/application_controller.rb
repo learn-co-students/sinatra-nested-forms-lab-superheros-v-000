755ffd20d3d36d89b2fa32f18b85851d9b98eeda
Christian Whitesides <whitesidescr@gmail.com>
2016-06-21 15:15:46 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,38 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    get '/' do
-      erb :super_hero
+  get '/' do
+    erb :super_hero
+  end
+
+  post '/team' do
+    @team = Team.new(params[:team])
+
+    params[:team][:members].each do |details|
+      Superhero.new(details)
     end
 
-    post '/team' do
-      @team = Team.new(params[:team])
+    @members = Superhero.all
 
-      params[:team][:members].each do |details|
-        Superhero.new(details)
-      end
+    erb :team
+  end
 
-      @members = Superhero.all
-      
-      erb :team
-    end
+  # This is the solution provided by the lesson -- no models necessary.
+  # post '/teams' do
+  #   @team_name = params[:team][:name]
+  #   @team_motto = params[:team][:motto]
+  #   @hero_name = []
+  #   @hero_power = []
+  #   @hero_bio = []
+  #   @team_members = params[:team][:members]
+  #   @team_members.each do |hero|
+  #     @hero_name << hero[:name]
+  #     @hero_power << hero[:power]
+  #     @hero_bio << hero[:bio]
+  #   end
+
+  #   erb :team
+  # end
 end
