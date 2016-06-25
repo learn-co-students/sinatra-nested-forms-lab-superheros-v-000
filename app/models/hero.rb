1d130c0585e94770fb47320fdc191c3e96718ffc
kaileeagray <kaileeagray@gmail.com>
2016-06-25 15:48:43 -0500

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,19 @@
+class Hero
+  attr_accessor :name, :power, :biography
+  HEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @biography = args[:bio]
+    HEROES << self
+  end
+
+  def self.all
+    HEROES
+  end
+
+  def self.clear
+    HEROES.clear
+  end
+end
