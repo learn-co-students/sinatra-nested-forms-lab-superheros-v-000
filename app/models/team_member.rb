0e3f3f6e8b5b5d02b2f92080366954f953c21250
arturusfury <bri.reynolds@gmail.com>
2016-07-28 12:43:45 -0500

    Done.

---
+++ app/models/team_member.rb
@@ -0,0 +1,18 @@
+# Team Member class
+class TeamMember
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
