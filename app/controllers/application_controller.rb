f88eadc55a81e92c9a6c0cc9670c8fc544632874
Jesse Novotny <jessenovotny@gmail.com>
2016-08-08 10:45:20 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+      erb :super_hero
+    end
+
+    post '/' do
+      @team = Team.create(name: params[:team][:name], motto: params[:team][:motto])
+      params[:team][:heros].map do |hero_details|
+        Hero.create(hero_details)
+      end
+
+      erb :team
+    end
 
 end
