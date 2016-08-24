488a3a7b5786cb03061d20d17da77cf31cdf48c8
ThisIsTyDell <milleb94@gmail.com>
2016-08-23 22:28:28 -0500

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,9 @@ class App < Sinatra::Base
     post '/teams' do
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
-      @hero_name = []
-      @hero_power = []
-      @hero_bio = []
+     
       @team_members = params[:team][:members]
-      @team_members.each do |hero, attrib|
-        @hero_name << attrib[:name]
-        @hero_power << attrib[:power]
-        @hero_bio << attrib[:bio]
-      end
+      
       erb :team
     end
 
