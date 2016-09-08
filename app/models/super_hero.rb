d6827eefd7b60101618f119b2a89b6b3c700ecf3
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-08 20:58:50 +0000

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,21 @@
+class SuperHero
+  attr_accessor :name, :power, :bio
+
+  @@super_heroes = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    @@super_heroes << self
+  end
+
+  def self.all
+    @@super_heroes
+  end
+
+  def self.clear
+    @@super_heroes = []
+  end
+
+end
