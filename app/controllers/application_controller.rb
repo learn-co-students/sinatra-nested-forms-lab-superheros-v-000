e565b4f7a55e3ca8796f3ee48ced602a41df6067
BriOD <briodonoghue@gmail.com>
2016-09-26 16:00:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class App < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
 
-
   get '/' do
     erb :super_hero
   end
 
   post '/teams' do
+
     @team_name = params[:team][:name]
     @team_motto = params[:team][:motto]
     @hero_name = []
@@ -20,10 +20,9 @@ class App < Sinatra::Base
       @hero_name << hero[:name]
       @hero_power << hero[:power]
       @hero_bio << hero[:bio]
+    end
 
     erb :team
   end
-
-end
 
 end
