cbb0c57ab8bdb0d9fdc93db571b778575acdc12a
Scott Savage <snsavage@gmail.com>
2016-10-28 21:38:45 -0400

    Pass all lab specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      params[:team][:member].each do |member|
+        Hero.new(member)
+      end
+
+      @team = Team.new(params[:team])
+      @heroes = Hero.all
+
+      erb :team
+    end
 end
