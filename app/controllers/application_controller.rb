0108cf65d3c971c110c7639d4c06d2f885e46f4f
Ray Mulligan <rmullig2@optonline.net>
2016-11-26 20:03:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :"../views/super_hero"
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+      @member1 = Member.new(params[:team][:members][0])
+      @member2 = Member.new(params[:team][:members][1])
+      @member3 = Member.new(params[:team][:members][2])
+      erb :"../views/team"
+    end
 
 end
