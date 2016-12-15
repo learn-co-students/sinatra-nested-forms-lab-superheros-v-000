d603b531d7cfc4be99580b33ad220f2306244063
mmelmer <mikemelmer@gmail.com>
2016-12-14 21:38:41 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,30 @@
 require 'sinatra/base'
+require './config/environment'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @team_members = []
+      @hero_names = []
+      @hero_powers = []
+      @hero_biographies = []
+      @heroes = params[:team][:hero]
+      @heroes.each do |hero|
+        @hero_names << hero[:name]
+        @hero_powers << hero[:power]
+        @hero_biographies << hero[:biography]
+      end
+      @count = @heroes.size.to_i
+      erb :team
+    end
 
 end
---
+++ app/models/team.rb
@@ -0,0 +1,10 @@
+class Team
+
+  attr_reader :name, :motto
+
+  def initialize(args)
+    @name = args[name]
+    @motto = args[motto]
+  end 
+
+end
