e2062fa1618a516eda6502f315dfa8b875c4214d
Krishel Lasam <krishel.lasam@gmail.com>
2017-01-09 11:15:53 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,18 @@
+class Team
+  attr_accessor :name, :motto
+  @@teams = []
+
+  def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
+    @@teams << self
+  end
+
+  def self.all
+    @@teams
+  end
+
+  def self.clear
+    @@teams.clear
+  end
+end
