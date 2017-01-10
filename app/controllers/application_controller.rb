0f09c40ed6645f12d4fc4dfa4f8edb0d279fe999
Mary Umoh <umohm12@gmail.com>
2017-01-10 23:28:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      @team = (params[:team][:name], params[:team][:motto])
       @member1 = (params[:team][:members][0][:name], params[:team][:members][0][:power], params[:team][:members][0][:bio])
       @member2 = (params[:team][:members][1][:name], params[:team][:members][1][:power], params[:team][:members][1][:bio])
       @member3 = (params[:team][:members][2][:name], params[:team][:members][2][:power], params[:team][:members][2][:bio])
