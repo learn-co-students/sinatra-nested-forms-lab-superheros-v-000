4c294d42acefd647626d476b5d63914bf03ab132
bortch89 <bortch@gmail.com>
2017-04-03 19:25:31 +0200

    Done.

---
+++ app/models/member.rb
@@ -0,0 +1,22 @@
+class Member
+
+@@all = []
+
+  attr_accessor :name, :power, :bio
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+end
