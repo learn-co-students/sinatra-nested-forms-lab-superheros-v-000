bda466d3902eb008d844609d695d06ee9231cbaf
Nadine Fischoff <nadine.khattak@gmail.com>
2017-05-04 09:58:51 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,30 @@
+class Hero
+
+  attr_reader :name, :power, :bio
+
+  @@heroes = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+
+    @@heroes << self
+
+  end
+
+  def self.all
+
+    @@heroes
+
+  end
+
+  def self.clear
+
+    @@ships = []
+
+  end
+
+
+
+end
