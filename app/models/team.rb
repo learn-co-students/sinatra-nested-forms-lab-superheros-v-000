85446bdd65331fbf80f2d2b6fe58beb8bd74b222
Alec Alba <alecgalba@gmail.com>
2017-09-01 17:03:57 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,15 @@
+class Team
+  attr_accessor :name, :motto, :members
+  @@all = []
+
+  def initialize(name, motto)
+    @name = name
+    @motto = motto
+    @members = []
+  end
+
+  def self.all
+    @@all
+  end
+  
+end
