fa7d22e0454dbdd3edde908f142d23239b44777e
CodeDad70 <eric.strickler@gmail.com>
2017-09-05 21:16:48 -0600

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,20 @@
+class Superhero
+  attr_reader :name, :power, :biography
+
+  SUPERHEROES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    SUPERHEROES << self
+  end
+
+  def self.all
+    SUPERHEROES
+  end
+  
+
+
+
+end
