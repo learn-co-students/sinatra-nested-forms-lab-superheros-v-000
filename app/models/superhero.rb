5e1564a2f9503dd43419e13da83c5e9d0997821c
Amberly <armstrong.amberly@gmail.com>
2017-09-21 10:41:45 -0400

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,17 @@
+class Superhero
+  attr_accessor :name, :power, :biography
+
+  SUPERHEROES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    SUPERHEROES << self
+  end
+
+  def self.all
+    SUPERHEROES
+  end
+
+end
