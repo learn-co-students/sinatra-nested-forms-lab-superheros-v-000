1e720dd21395d5f0365f322bbe9a24db66a7351e
Derek  Pelt <derekpelt@gmail.com>
2017-10-03 08:05:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,16 @@ class App < Sinatra::Base
     post '/team' do
 
       @team = Team.new(params[:team])
-      @hero1 = Hero.new(name: params[:member1_name], power: params[:member1_power], bio: params[:member1_bio] )
-      @hero2 = Hero.new(name: params[:member2_name], power: params[:member2_power], bio: params[:member2_bio] )
-      @hero3 = Hero.new(name: params[:member3_name], power: params[:member3_power], bio: params[:member3_bio] )
+      #@hero1 = Hero.new(name: params[:member1_name], power: params[:member1_power], bio: params[:member1_bio] )
+      #@hero2 = Hero.new(name: params[:member2_name], power: params[:member2_power], bio: params[:member2_bio] )
+      #@hero3 = Hero.new(name: params[:member3_name], power: params[:member3_power], bio: params[:member3_bio] )
+      #binding.pry
+      params[:team][:members].each do |hero|
+        Hero.new(hero)
+      end
+
+      @heros = Hero.all
+
 
       #binding.pry
 
