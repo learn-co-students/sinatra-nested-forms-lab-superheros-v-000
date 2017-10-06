b740ca1869d3a2d69c526e4ab1c1a82bf3f02d07
daniellecroxton <danikrist@gmail.com>
2017-10-05 22:42:26 -0600

    Done.

---
+++ app/controllers/app.rb
@@ -0,0 +1,22 @@
+# require_relative './config/environment'
+
+  class App < Sinatra::Base
+
+    get '/' do
+
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:members].each do |details|
+        Member.new(details)
+      end
+
+      @members = Member.all
+
+       erb :team
+    end
+
+end
