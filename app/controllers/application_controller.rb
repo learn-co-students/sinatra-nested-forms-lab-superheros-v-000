1380e0af35c91489f6b821efba0b6251e1c6e933
ndalcin <dalcin.nicole@gmail.com>
2017-10-13 07:06:22 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,15 @@ class App < Sinatra::Base
     post '/teams' do
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
-      @member1_name = params[:member1_name]
-      @member1_power = params[:member1_power]
-      @member1_bio = params[:member1_bio]
-      @member2_name = params[:member2_name]
-      @member2_power = params[:member2_power]
-      @member2_bio = params[:member2_bio]
-      @member3_name = params[:member3_name]
-      @member3_power = params[:member3_power]
-      @member3_bio = params[:member3_bio]
-      
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @members = params[:team][:members]
+      @members.each do |hero|
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
+      end
       erb :team
     end
 
