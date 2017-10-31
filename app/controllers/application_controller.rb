d0470af2f7338d5c7ed3c952a518bca4bbaf629b
Richard Steinbach <steinbach.dick@gmail.com>
2017-10-31 14:47:41 -0700

    sinatra-nested-forms-lab-superheros-v-000

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :'/team'
+    end
 
+    post '/teams' do
+        #binding.pry
+      @team = Team.new(params[:team])
+    #this will create team from the model
+
+  #binding.pry
+      params[:team][:members].each do |hero|
+      Superhero.new(hero)
+      end
+      @hero = Superhero.all
+      erb :'/show'
+    end
 end
