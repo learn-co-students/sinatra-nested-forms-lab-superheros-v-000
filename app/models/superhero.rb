306e6fc28226f55625eb9b4b5a045ca6e5521512
github username <github email address>
2017-11-02 21:20:27 -0700

    first commit

---
+++ app/models/superhero.rb
@@ -0,0 +1,17 @@
+class Superhero
+
+    attr_accessor :name, :power, :bio
+
+    SUPERHEROES = []
+
+    def initalize(params)
+        @name = params[:name]
+        @power = params[:power]
+        @bio = params[:bio]
+        SUPERHEROES << self
+    end
+
+    def self.all
+        SUPERHEROES
+    end
+end
