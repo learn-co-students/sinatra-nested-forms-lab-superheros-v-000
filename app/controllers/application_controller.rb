77917fc4c8a4a285c475be22295f0a1c6ea72c56
miriam <miriam5000@gmail.com>
2017-11-19 22:07:54 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,36 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
-end
+    get '/' do
+    	render superhero form 
+    	erb :super_hero
+    end 
+
+    post '/teams' do
+    	submits the form 
+    	erb :team
+    end 
+
+
+end 
+
+
+# team = 
+#  {name=>
+#   motto =>
+#   member_1 {
+#   	name =>
+#   	power =>
+#   	bio =>
+#   }
+#   member_2 {
+#   	name =>
+#   	power =>
+#   	bio =>
+#   }
+#   member_3 {
+#   	name =>
+#   	power =>
+#   	bio =>
+#   }
+#  }
