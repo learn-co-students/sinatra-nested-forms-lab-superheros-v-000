69f73391097a3081d4778613f3eb620b58f4c237
dbfarms <matt.hunger@gmail.com>
2017-12-19 14:39:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,15 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+    get '/' do 
+      erb :super_hero  #:'../views/super_hero' 
+    end 
+    
+    post '/teams' do 
+      #binding.pry
+      @team = params 
+      
+      
+      erb :team   #:'../views/team'      
+    end 
 end
