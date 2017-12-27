37cc0c92dbadd963bef0fbb271d1e6b3c22fd493
Lara Sanders <larasanders@gmail.com>
2017-12-27 01:36:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,28 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :'super_hero'
+      erb :'/super_hero'
     end
 
-# get '/new' do
-#   erb :'views/super_hero'
-# end
+get '/teams' do
+  erb :'super_hero'
+end
 
 post '/teams' do
   @team = Team.new(params[:team])
-  params[:team][:super_heros].each do |details|
+  params[:team][:super_heroes].each do |details|
     SuperHero.new(details)
   end
-  @super_heros = Super_hero.all
+  @super_heroes = Super_hero.all
   erb :'/team'
 end
 end
+
+# <% @team_members.each.with_index do |hero, index| %>
+#   <h2>Hero Name: <%= @hero_name[index] %></h2>
+#   <p>
+#     Hero power: <%= @hero_power[index] %>
+#     <br>
+#     Hero biography: <%= @hero_bio[index] %>
+#   </p>
+# <% end %>
