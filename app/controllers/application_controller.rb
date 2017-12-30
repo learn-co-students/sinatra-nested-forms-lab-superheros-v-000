1c7538f827c86c2b296b58fd89b23f7c3227f974
Sequina Caro <sccaro89@gmail.com>
2017-12-30 21:54:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
+require 'pry'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+   get '/' do
+     erb:'/super_hero'
+   end
+  get '/teams' do
+    erb:'/team'
+  end
 
-
+  post '/teams' do
+    @team = Team.new(params[:team])
+    params[:team][:members].each do |details|
+      SuperHero.new(details)
+    end
+    @super_heroes = SuperHero.all
+      erb:'/team'
+  end
 end
