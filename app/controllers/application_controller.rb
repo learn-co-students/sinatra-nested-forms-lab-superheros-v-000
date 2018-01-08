fec23a3323959bd1ec9874d21909d86a11913292
Steven Senkowski <scsenkowski@gmail.com>
2018-01-08 17:39:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,7 @@ class App < Sinatra::Base
         @team = Team.new(name: params[:team][:name],motto: params[:team][:motto])
 
         hero = params[:team][:members]
-
         @super_heroes = hero.map { |hero_params| SuperHero.new({name: hero_params[:name], power: hero_params[:power], bio: hero_params[:bio]})}
-        binding.pry
-
       erb :team
     end
 
