96606f0e885917acc3c2e0cead1631bc870d0b8f
churchwerks <steven.lennis@icloud.com>
2018-01-26 11:59:09 -0600

    initial Attempt

---
+++ app/models/teamold.rb
@@ -0,0 +1,12 @@
+# class Team
+#   attr_accessor :name, :motto
+#
+#   def self.all
+#     @@all ||= []
+#   end
+#
+#   def initialize(args)
+#     @name  = args[:name]
+#     @motto = args[:motto]
+#   end
+# end
