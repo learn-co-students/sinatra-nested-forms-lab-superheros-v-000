dca97245deafe7e8b639e7082fb90fea9f0b6bdd
funemployeddad <funemployeddad@gmail.com>
2018-02-16 17:02:44 -0700

    added :team_id to the CreateHeroes migration file

--- db/migrate/create_heroes.rb
+++ db/migrate/create_heroes.rb
@@ -1,9 +1,10 @@
-class CreateHeroes < ActiveRecord::Migration[4.2]
+class CreateHeroes < ActiveRecord::Migration
   def change
     create_table :heroes do |t|
       t.string  :name
       t.string  :power
       t.string  :bio
+      t.integer :team_id
     end
   end
 end
