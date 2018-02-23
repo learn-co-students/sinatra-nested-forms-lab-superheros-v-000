25f4ca8578f0bf308177f0a934fd9df1d2e6edd0
Blakeley Bond <burrick2003@yahoo.com>
2018-02-23 03:15:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,25 @@ require './config/environment'
 require 'sinatra/base'
 
 class App < Sinatra::Base
-  set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") } #needs this since not app.rb in the root
 
   get '/' do
     erb :super_hero
   end
 
-  post '/teams' do
+  post '/team' do
+    @heronames=[] #empty arrays to be filled with params for the 3 heroes
+    @heropowers=[]
+    @herobios=[]
+    @teamname=params[:team][:name]
+    @teammotto=params[:team][:motto]
+
+    @teammembers=params[:team][:members]
+        @teammembers.each do |member|
+          @heronames << member[:name]
+          @heropowers << member[:power]
+          @herobios << member[:bio]
+        end
 
   erb :team
   end
