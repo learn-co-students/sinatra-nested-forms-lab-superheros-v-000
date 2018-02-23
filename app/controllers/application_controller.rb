3db1a10166bce51e2e2ea0f47870b96b187d12f0
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-23 19:27:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:members].each do |member_info|
+        Hero.new(member_info)
+      end
+      @members = Hero.all
+
+      erb :team
+    end
 end
