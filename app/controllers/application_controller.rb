bff47853dafc1f2fd9139dea2954d602ffcdc514
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-28 16:42:02 -0500

    Add nested form of team and superheroes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,26 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+
+      @hero_name = []
+      @hero_power = []
+      @hero_biography = []
+      @team_mates = params[:team][:heroes]
+      @team_mates.each do |hero|
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_biography << hero[:biography]
+      end
+
+      erb :team
+    end
+
 
 end
