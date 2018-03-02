3898ee66bc1e5496a6418980089f27d595165f83
jljardon <github email address>
2018-03-02 03:24:17 -0500

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,17 @@
+class Hero
+
+   attr_accessor :name, :power, :biography
+
+   @@all = []
+
+   def initialize(params)
+      @name = params[:name]
+      @power = params[:power]
+      @biography = params[:biography]
+      @@all.push(self)
+   end
+
+   def self.all
+      @@all
+   end
+end
