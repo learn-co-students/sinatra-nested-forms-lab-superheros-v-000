7083174cbf60ab02fc51dc1a64c0412deafbe786
Obafemi Fadairo <ofpupp@princeton.edu>
2018-03-10 00:11:15 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,23 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+    erb :super_hero
+  end
+  
+  post '/teams' do
+    @name = params["team"]["name"]
+    @motto = params["team"]["motto"]
+    @member_1_name = params["team"]["members"][0]["name"]
+    @member_2_name = params["team"]["members"][1]["name"]
+    @member_3_name = params["team"]["members"][2]["name"]
+    @member_1_power = params["team"]["members"][0]["power"]
+    @member_2_power = params["team"]["members"][1]["power"]
+    @member_3_power = params["team"]["members"][2]["power"]
+    @member_1_bio = params["team"]["members"][0]["bio"]
+    @member_2_bio = params["team"]["members"][1]["bio"]
+    @member_3_bio = params["team"]["members"][2]["bio"]
+    erb :team
+  end
 
 end
