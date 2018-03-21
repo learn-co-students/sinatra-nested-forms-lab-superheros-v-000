6c3346d71aebdb0544fa1695bff68f6502b8e285
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-21 19:29:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      #shows to data
+      #instantiates instance variables and creates objects
+      @team = Team.new(params)
+
+      params[:team][:heroes].each do |details|
+        Hero.new(details)
+      end
+
+      @heroes = Hero.all
+      binding.pry
+      erb :team
+    end
+
+
 
 end
