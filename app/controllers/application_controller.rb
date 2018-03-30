94725d929b650168d3d478f125aeca0a46e0ccd9
Bertrand <bertrand.chevalier@gmail.com>
2018-03-29 22:28:01 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/team' do
+        # binding.pry
+        @params
+        erb :team
+    end
+
 end
