612d76e2bedb90910ba4f00c474958295d1fe2f7
Sam. O <samrey2018@gmail.com>
2018-04-12 05:43:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+ # when user get to our page... Line 8 route fires.
  get '/' do
     erb :super_hero
  end
 
  post '/teams' do
     erb :teams
+    # calls the file teams
  end
 
 end
