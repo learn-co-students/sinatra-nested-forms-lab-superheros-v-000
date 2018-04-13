32ecf6043307728ec7d9e3ce578f780af382d43e
John O'Connor <oconnojb@gmail.com>
2018-04-13 16:54:38 +0000

    Done.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,14 +1,8 @@
 class Team
-  attr_accessor :name, :motto, :heroes
-  @@all = []
+  attr_accessor :name, :motto
 
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 end
