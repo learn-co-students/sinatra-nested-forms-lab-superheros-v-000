b7971ae2860ab46d229c796cb7a45de6230d083f
Denise Pendleton <dpendl1@hotmail.com>
2018-04-16 04:21:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do
+      erb :super_hero
+          end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+      #  binding.pry
+      params[:team][:members].each do |details|
+
+        member = Member.new(details)
+          # binding.pry
+        end
+
+      @members = Member.all
+        erb :team
+
+    end
 
 
 end
---
+++ app/models/member.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Member
+  attr_reader :name, :power, :biography
+
+  MEMBERS=[]
+
+  def initialize(params)
+    # binding.pry
+    @name=params[:name]
+    @power=params[:power]
+    
+    @biography=params[:biography]
+    
+    MEMBERS << self
+    
+  end
+
+  def self.all 
+    MEMBERS
+  end
+
+  def self.clear
+    MEMBERS.clear
+  end
+
+end
