57e412ca52aeade4bb925bb580bd322691351ede
Man Leung <lepusleung@gmail.com>
2018-04-16 21:40:34 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,18 @@
+class Hero
+
+  attr_accessor :name, :power, :bio
+
+  Heroes = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    Heroes << self
+  end
+
+  def self.all
+    Heroes
+  end
+
+end
