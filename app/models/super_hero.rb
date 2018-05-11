3e40a7e717baac061c61333decb2bbf5cffb3b06
Trevor Seitz <trevinla@gmail.com>
2018-05-11 17:58:33 +0000

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,24 @@
+# initializes new Super Hero
+
+class Superhero
+
+  attr_accessor :name, :power, :bio
+
+  SUPERHEROES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    SUPERHEROES << self
+  end
+
+  def self.all
+    SUPERHEROES
+  end
+
+  def self.clear
+  SUPERHEROES.clear
+end
+
+end
