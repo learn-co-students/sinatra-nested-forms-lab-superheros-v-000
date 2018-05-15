f65b1c69bab92e40fa8da96e62a5ceeb0b6c3207
David Ritchey <ritchey0713@gmail.com>
2018-05-15 04:25:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
     post '/teams' do
     @team = Team.new(params[:team])
     
-    params[:team][:member].each do |details|
+    params[:team][:members].each do |details|
       Hero.new(details)
     end 
     
