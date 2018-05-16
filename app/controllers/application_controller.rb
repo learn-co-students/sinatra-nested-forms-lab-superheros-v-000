35772218e36548800f55364f49df8c73b95c7152
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-16 23:52:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :'super_hero'
+    end
+
+    post '/teams' do
+      @team = Team.create(name: params[:team][:name], motto: params[:team][:motto])
+
+      params[:team][:members].each do |details|
+        @member = Member.new(details)
+        @member.team = @team
+        @member.save
+      end
+
+      erb :team
+    end
+
 
 end
