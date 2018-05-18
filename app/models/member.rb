b1a9f0adac3346208d19f3b24277b1e8ddb66afc
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-05-18 17:59:12 -0400

    Done.

---
+++ app/models/member.rb
@@ -0,0 +1,20 @@
+class Member
+  attr_reader :name, :power, :biography
+
+  MEMBERS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    MEMBERS << self
+  end
+
+  def self.all
+    MEMBERS
+  end
+
+  def self.clear
+    MEMBERS.clear
+  end
+end
