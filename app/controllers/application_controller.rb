62d2682211b6cfb525fac8a79bea0d5a9b139c73
Kriti Rai <kriti.rai@gmail.com>
2018-05-22 03:42:25 +0000

    Substituted hardcode with iterating over members array to create arrays of names, powers and bios and saved them in corresponding instance variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,26 +5,26 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :team
+      erb :super_hero
     end
 
     post '/teams' do
       team = params["team"]
       @team_name = team["name"]
       @team_motto = team["motto"]
-      
-      members = params["team"]["members"]
-      @member1_name = members[0]["name"]
-      @member1_power = members[0]["power"]
-      @member1_bio = members[0]["bio"]
-      @member2_name = members[1]["name"]
-      @member2_power = members[1]["power"]
-      @member2_bio = members[1]["bio"]
-      @member3_name = members[2]["name"]
-      @member3_power = members[2]["power"]
-      @member3_bio = members[2]["bio"]
 
-      erb :super_hero
+      members = params["team"]["members"]
+      @member_names = []
+      @member_powers = []
+      @member_bios = []
+
+      members.each do |member|
+        @member_names << member[:name]
+        @member_powers << member[:power]
+        @member_bios << member[:bio]
+        end
+
+      erb :team
     end
 
 end
