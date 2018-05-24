741480ba18bca42abe840e8f700bc6f03e5f0b10
Victoria Meng <nights1ststar@protonmail.com>
2018-05-24 19:09:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,13 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      @team = Team.new(params[:team])
+
+      @team.members.each do |hero|
+        Hero.new(hero)
+      end
+
+      @heroes = Hero.all
 
       erb :team
     end
