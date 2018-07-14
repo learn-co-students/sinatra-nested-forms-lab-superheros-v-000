282aa8dddfbe09831abe778b4bd495637384f8c7
Kayla Reopelle <kreopelle@gmail.com>
2018-07-14 19:33:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,17 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
+    post '/teams' do
+      @team = Team.new(params[:team][:name], params[:team][:motto])
+
+      params[:team][:members].each do |details|
+        Member.new(details[:name], details[:power], details[:bio])
+      end
+
+      @members = Member.all
+
+      erb :team
+    end
+
 
 end
