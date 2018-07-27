140fa9115a28a2eb6cc5980fd0e143f1653c7f10
Christopher P. Dundon <chris.dundon@gmail.com>
2018-07-27 18:17:01 -0400

    lab complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+		@@path = File.join(root, "../views/")
     set :views, Proc.new { File.join(root, "../views/") }
+    #set :views, Proc.new { File.join(root, "./views/") }
+		
 
+		get '/' do
+			@path = @@path
+			erb :index
+		end
+
+		post '/team' do
+			@team_name = params[:team][:name]
+			@team_motto = params[:team][:motto]
+
+			@members = params[:team][:members]
+			erb :team
+		end
 
 end
