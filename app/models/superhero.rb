c8c4c9c55d6df4c1b7a2d9be0e1f2633597be9a2
Emily S <emily.sugano@gmail.com>
2018-08-09 19:41:11 +0000

    Done.

---
+++ app/models/superhero.rb
@@ -0,0 +1,17 @@
+class Superhero
+  attr_reader :name, :power, :bio
+
+  SUPERHEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    SUPERHEROES << self
+  end
+
+  def self.all
+    SUPERHEROES
+  end
+
+end
