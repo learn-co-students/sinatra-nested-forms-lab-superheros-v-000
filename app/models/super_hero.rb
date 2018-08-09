2accb8c7fca5971e5868f0a49deb5888473de60d
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-09 01:06:41 +0000

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,22 @@
+class Superhero
+  attr_accessor :name, :power, :bio
+
+  @@superheroes = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    @@superheroes << self
+  end
+
+  def self.all
+    @@superheroes
+  end
+
+  def self.clear
+    @@superheroes = []
+  end 
+
+
+end
---
+++ app/models/team.rb
@@ -0,0 +1,19 @@
+class Team
+  attr_accessor :name, :motto
+
+  @@teams = []
+
+  def initialize(args)
+    @name = args[:name]
+    @motto = args[:motto]
+    @@teams << self
+  end
+
+  def self.all
+    @@teams
+  end
+
+  def self.clear
+    @@teams = []
+  end 
+end
