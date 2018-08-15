1a7efd2c043e23b7b537129e6c83375b5f2be5fa
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-08-15 02:18:35 +0000

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,18 @@
+class SuperHero
+
+  attr_accessor :name, :power, :bio
+
+  SUPERHEROS = []
+
+  def initialize(name, power, bio)
+    @name = name
+    @power = power
+    @bio = bio
+    SUPERHEROS << self
+  end
+
+  def self.all
+    SUPERHEROS
+  end
+
+end
