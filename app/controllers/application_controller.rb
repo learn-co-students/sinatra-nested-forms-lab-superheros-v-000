19fbdedfcba07289c013f03bac2e4d5865a31616
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 23:41:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :new
+    end
+
+    post '/teams' do
+      erb :team
+    end
 
 end
