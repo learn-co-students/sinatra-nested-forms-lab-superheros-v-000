6e5833a39e6c3b84159e2ebf831638bd2fb3f526
Alexandra Fren <alexandra.fren@gmail.com>
2018-08-22 14:29:30 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,20 @@
+class Team
+  attr_accessor :name, :motto
+
+	@@all = []
+
+	def initialize(args)
+		@name = args[:name]
+		@motto = args[:motto]
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.clear
+		@@all = []
+	end
+
+end
