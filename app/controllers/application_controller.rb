ce9e4ba573c8cad7c961bfa64053cd4231f8e932
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-11 18:23:02 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :root
+    end
+
+    post '/teams' do
+      erb :teams
+    end
 
 end
