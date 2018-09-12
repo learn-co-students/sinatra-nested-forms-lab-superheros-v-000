c8be8622439f0d23ac4f41e3d36092e970d170bb
Madeline Stark <madeline.stark1@gmail.com>
2018-09-12 21:06:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,22 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+
+    post '/teams' do
+      @team = Team.new(params[:team]) #storing in variable gives show.erb something to call .name and other methods on
+
+      params[:team][:heroes].each do |details|
+        Superhero.new(details)
+      end
+
+      @heroes = Superhero.all #can then call on with index number
+
+      erb :team
+    end
+
 
 end
