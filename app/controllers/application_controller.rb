0f4b74ffb3971122ce1edeb62b186aab8d490cc2
amitnyc83 <amit_nyc@live.com>
2018-10-02 15:13:48 -0400

    finish lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,29 @@
 require 'sinatra/base'
 
+
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
+
+
+  get '/' do
+
+    erb :'../views/super_hero'
+  end
+
+
+
+   post '/teams' do
+    @team = Team.new(params[:team][:name], params[:team][:motto])
+
+    @members = params[:team][:member].each do |member_info|
+       Member.new(member_info[:name], member_info[:power], member_info[:bio])
+    end
+
+
+
+     erb :'../views/team'
+  end
 
 
 end
---
+++ app/models/member.rb
@@ -0,0 +1,21 @@
+class Member
+
+
+  attr_accessor :name, :power, :bio
+
+  @@all = []
+
+  def initialize(name, power, bio)
+    @name= name
+    @power= power
+    @bio= bio
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
+end
