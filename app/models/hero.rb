11ae0e85fc142337758346041739f3eb527d3fad
Sushi Chanrai <sushi@pathworks.co>
2018-10-11 15:26:52 -0400

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,17 @@
+class Hero
+  attr_reader :name, :power, :biography
+
+ HERO = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    HERO << self
+  end
+
+  def self.all
+    HERO
+  end
+
+end
