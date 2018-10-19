9e95a179fed82750ba8af367529e1165411c30e6
Brandon Green <brandon@brandonleeproductions.com>
2018-10-19 00:56:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    # get '/team' do
+    #   erb :team
+    # end
+
+    post '/teams' do
+      @team = Team.new(params[:member])
+
+      params[:team][:members].each do |details|
+        Member.new(details)
+      end
+
+      @members = Member.all
+
+      erb :team
+
+end
 end
