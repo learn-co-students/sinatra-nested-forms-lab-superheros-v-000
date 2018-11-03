572c42a20719c6a9beacf7ac709152c11c930fd3
Brian Birdwell <bbirdwell@gmail.com>
2018-11-03 23:39:20 +0000

    add first controller method and form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :create
+    end
+
+    post '/teams' do
+      @team = Team.new(team[:params])
+    end
 
 end
