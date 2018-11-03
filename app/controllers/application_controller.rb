29a7617fecafb363d2db5b368415624b83dacd62
Sherry Martin <sm674@cornell.edu>
2018-11-03 21:10:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,33 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @hero_power = []
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @whole_team = params[:team][:members]
+      @whole_team.each do |i_hero|
+      @hero_name << i_hero[:name]
+      @hero_bio << i_hero[:bio]
+      @hero_power << i_hero[:power]
+    end
+
+      # Initial attempt without individual models:
+      # @team = Team.new(params[:team][:name], params[:team][:motto])
+      #
+      # params[:team][:members].each do |content|
+      #   Member.new(content[:name], content[:power], content[:bio])
+      # end
+      #
+      # @members = Member.all
+
+      erb :team
+    end
 end
