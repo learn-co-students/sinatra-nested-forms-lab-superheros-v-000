55a665c62ce254e3e71c86bc8f6166458c3b19dc
Brian Birdwell <bbirdwell@gmail.com>
2018-11-04 06:09:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,15 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @team = Team.new(team[:params])
+      @team = Team.new(params[:team])
+
+      params[:team][:members].each do |details|
+        Member.new(details)
+      end
+
+      @members = Member.all
+
+      erb :team
     end
 
 end
