b0345977edd90b0300424fc68e76426a52b5dbae
Harley Rayner <harleymteden@gmail.com>
2018-11-27 20:33:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,25 +11,21 @@ class App < Sinatra::Base
     post '/teams' do
 
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-binding.pry
-      params[:team][:heroes].each_with_index do |hero_data, index|
+
+      #iterates over the params hash (member1, member2, member3)
+      params[:team][:heroes].each do |member|
+
         hero = Hero.new
-        index += 1
-        # binding.pry
-        hero.name = hero_data["member#{index}_name".to_sym]
-        hero.power = hero_data["member#{index}_power".to_sym]
-        hero.bio = hero_data["member#{index}_bio".to_sym]
+
+        hero.name = member["name"]
+        hero.power = member["power"]
+        hero.bio = member["bio"]
+
         @team.heroes << hero
-        # binding.pry
-        # binding.pry
+
       end
-      @team
-        # binding.pry
+
       erb :show
     end
-
-    # get '/teams' do
-    #   erb :show
-    # end
 
 end
