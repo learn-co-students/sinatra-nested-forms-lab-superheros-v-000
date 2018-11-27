2e70d08d1245d3fbc9ecc3e504b9d79e8086b8dd
Harley Rayner <harleymteden@gmail.com>
2018-11-27 05:08:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
+
+    post '/teams' do
+      # binding.pry
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+
+      params[:team].each do |hero_data|
+        binding.pry
+        hero = Hero.new
+        hero.name = hero_data[:name]
+        hero.power = hero_data[:power]
+        ship.bio = hero_data[:bio]
+        @team.heroes << hero
+      end
+
+      erb :show
+    end
 
 end
