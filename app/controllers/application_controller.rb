1772bd378e3c00b97f61814bff47321baf036cec
yuanxizhang <yuanxi.zhang@gmail.com>
2019-01-28 13:10:11 -0800

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+    	erb :index
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+      params[:team][:members].each do |m|
+        Member.new(m)
+      end
+      @members = Member.all
+      erb :team
+    end
+
 
 end
