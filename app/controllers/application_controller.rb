4795faf44ced094dcc306899e3bf3fa05a778e34
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-07 21:32:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-
+        erb :super_hero
     end
 
+    get '/teams' do
+        @team = Team.new(args[:team])
+
+        params[:team][:superheroes].each do |details|
+            Superhero.new(details)
+        end
+
+        @superheroes = Superhero.all
+
+        erb :team
+    end
 end
