d12315103af4d8f9c801a4eb536c34243a6bc1d1
Heidi Hlavinka <hchlav@gmail.com>
2019-03-27 18:58:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,15 @@ class App < Sinatra::Base
 
     post '/teams' do
       erb :team
+      # <!-- iterate through params - .each.with_index
+      # params[:team][:members].each.with_index do |value, index|
+      #   puts "#{index}: #{value}"
+      #   create hero name
+      #   create hero power
+      #   create hero bio
+      # end
+      # *look in Sinatra Nested Forms lab
+      # ?? iterate in this file?-->
     end
 
 
