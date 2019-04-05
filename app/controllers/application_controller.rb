e95f4aa5bfc8157aa2df8baefe4ca068a9466b3d
Laura Messa Travin <laura.travin@gmail.com>
2019-04-05 00:01:54 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,20 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
+    post '/new' do
+
+      @team.name= params[:team][:name]  #params['name']
+      @team.motto= params[:team][:motto]
+
+      params[:team][:members].each do |m|
+        Hero.new(m)
+      end
+
+      binding.pry
+      
+      @heros= Hero.all
+
+      erb  :team
+    end
+
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,10 @@
-class Hero
-  attr_accessor :name, :power, :bio
+class Team
+  attr_accessor :name, :motto
   @@all=[]
 
   def initialize(args)
     @name= args[:name]
-    @power= args[:power]
-    @bio=args['bio']
+    @power= args[:motto]
     @@all << self
   end
   def self.all
