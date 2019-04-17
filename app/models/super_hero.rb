3e3ecda8b22525a48362222b26b807b9a983f028
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-17 12:13:41 -0400

    WIP

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,13 +1,17 @@
-<h1>Create a Team and Heroes!</h1>
-<form action="/super_hero" method="post">
-  <div>Team Name: <input type="text" name="team[name]"></div>
-  <div>Team Motto: <input type="text" name="team[name]"></div>
-  <!-- <div>Pirate Height: <input type="text" name="pirate[height]"></div>
-  <div>Pirate Ship1 Name: <input type="text" name="pirate[ships][][name]" id="ship_name_1"></div>
-  <div>Pirate Ship1 Type: <input type="text" name="pirate[ships][][type]" id="ship_type_1"></div>
-  <div>Pirate Ship1 Booty: <input type="text" name="pirate[ships][][booty]" id="ship_booty_1"></div>
-  <div>Pirate Ship2 Name: <input type="text" name="pirate[ships][][name]" id="ship_name_2"></div>
-  <div>Pirate Ship2 Type: <input type="text" name="pirate[ships][][type]" id="ship_type_2"></div>
-  <div>Pirate Ship2 Booty: <input type="text" name="pirate[ships][][booty]" id="ship_booty_2"></div> -->
-  <input type="submit" value="Submit">
-</form>
+class SuperHero
+  attr_accessor :name, :hero_power, :hero_biography
+
+  SUPERHEROS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @hero_power= params[:hero_power]
+    @hero_biography= params[:hero_biography]
+
+    SUPERHEROS << self
+  end
+
+  def self.all
+   SUPERHEROS
+  end
+end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,1 +1,15 @@
-<h1>TEST TEST TEST</h1>
+class Team
+  attr_accessor :name, :team_motto
+
+  TEAMS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @:team_motto = params[:team_motto]
+    TEAMS << self
+  end
+
+  def self.all
+    TEAMS
+  end
+end
