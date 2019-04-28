bba5e9905ddaf8aa233b39b50206d632cb1f4224
Dan Hintz <dhintz@live.com>
2019-04-28 18:44:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,18 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
   get "/" do
-    erb :create_team
+    erb :super_hero
   end
   
   post "/teams" do
     @team = Team.new(params[:team])
     
     params[:team][:members].each do |member|
-      Member.new(member)
+      SuperHero.new(member)
     end
     
-    @members = Member.all
+    @heroes = SuperHero.all
+    
     erb :team
   end
   
