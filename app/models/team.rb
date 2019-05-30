9a25063d9c8639144c5b8849bc53d37a6f6664fa
Azhar Shah <azharshahstl@yahoo.com>
2019-05-30 23:29:03 +0000

    Done.

---
+++ app/models/team.rb
@@ -0,0 +1,19 @@
+class Team
+  
+  attr_accessor :name, :motto 
+  
+  @@teams = []
+  
+  def initialize(args)
+  @name = args[:name] 
+  @motto = args[:motto]
+  
+  @@teams << self 
+  end
+  
+  def self.all 
+    @@teams
+  end
+  
+  
+end
