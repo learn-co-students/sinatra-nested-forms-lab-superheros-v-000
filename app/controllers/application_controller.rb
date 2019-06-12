7a76cbaecb42977146a4addf3aee6d2ed1c112a0
Paul Fell <swellpf@gmail.com>
2019-06-12 15:03:15 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,21 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
+      #binding.pry
       erb :index
     end
 
     post '/teams' do
+
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-      members = params [:team][:members]
+      members = params[:team][:members]
+
       @superheroes = members.collect do |member_params|
+
         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
-      end
+    
+    end
+
       erb :team
     end
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,10 +1,17 @@
 class SuperHero
   attr_accessor :name, :power, :bio
 
+  @@all = []
+
   def initialize(args={})
     @name = args[:name]
     @power = args[:power]
     @bio = args[:bio]
     self.save
   end
+
+  def save
+    @@all << self
+  end
+
 end
