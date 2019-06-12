5eb2cd1254c5439f0defc412fb824bfa275ee8c9
Paul Fell <swellpf@gmail.com>
2019-06-12 14:32:14 -0400

    Done.

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,18 +1,10 @@
 class SuperHero
   attr_accessor :name, :power, :bio
 
-  def self.all
-    @@all ||= []
-  end
-
   def initialize(args={})
-    @name  = args[:name]
+    @name = args[:name]
     @power = args[:power]
-    @bio   = args[:bio]
+    @bio = args[:bio]
     self.save
-  end
-
-  def self.save
-    self.class.all << self
   end
 end
