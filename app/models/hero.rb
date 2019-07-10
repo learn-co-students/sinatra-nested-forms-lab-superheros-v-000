859af8cd2e4bc6e737336ac5b096a69955c2269f
Steven Crouse <sdcrouse91@gmail.com>
2019-07-10 02:57:17 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,16 @@
+class Hero
+  attr_accessor :name, :power, :bio
+  @@all = []
+  
+  def initialize(hero_traits)
+    @name = hero_traits[:name]
+    @motto = hero_traits[:power]
+    @bio = hero_traits[:bio]
+
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+end
