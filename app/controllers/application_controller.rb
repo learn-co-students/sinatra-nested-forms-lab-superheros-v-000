7c505a1eaa3d7aadb26f76e752dcfea6be384deb
Diapause <diapause@protonmail.com>
2019-07-31 18:20:17 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
-require 'sinatra/base'
-
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+
+    get '/' do
+      # 'hello world'
+      erb :index
+    end
 
 
 end
