8587a93a5851f7c266a695b7921fe3d85a35e7b0
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-10-01 15:13:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,23 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
+      #  binding.pry
+      post '/teams/' do
+
+        @team = Team.new(params[:name],params[:motto])
+        erb :'teams/team'
+    end
+
+
+
+
+
+
+
+
+
 
 end
