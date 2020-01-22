8823936835432c81d3855c7266a37e978885348c
Jonathan Fernandez <jfern20@gmail.com>
2020-01-22 03:31:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,33 @@ class App < Sinatra::Base
  
  
  post '/teams' do 
-   @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-   members = params[:team][:members]
-   
-   members.each do |member_params|
-     SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+   @team_name = params[:team][:name]
+   @team_motto = params[:team][:motto]
+   @hero_name = []
+   @hero_power = []
+   @hero_bio = [] 
+   @team_members = params[:team][:members]
+   @team_members.each do |hero|
+     @hero_name << hero[:name]
+     @hero_power << hero[:power]
+     @hero_bio << hero[:bio]
    end 
-   @super_heroes = SuperHero.all 
+   erb :team
+  end 
+ end 
+ 
+ 
+ 
+   #@team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+   #members = params[:team][:members]
+   
+   #members.each do |member_params|
+     #SuperHero.new({name: member_params[:name], power: member_params[:power], #bio: member_params[:bio]})
+   #end 
+   #@super_heroes = SuperHero.all 
    
    
-    erb :team
-   end 
+    #erb :team
+   #end 
 
-end
+#end
