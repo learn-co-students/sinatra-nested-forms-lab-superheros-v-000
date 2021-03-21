5534ebd1b89c26d4046359574d3ad37ea97e59c3
github username <github email address>
2021-03-21 12:53:45 -0400

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,19 @@
+class Hero 
+   attr_reader :name, :power, :biography
+
+   HEROES = []
+
+   def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @biography = args[:biography]
+    HEROES << self
+   end
+
+   def self.all
+    HEROES
+   end
+
+
+
+end
